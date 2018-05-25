1935bdd582d9e035bfa47e8924bbb3612ed60756
hutchwhite <hutchjwhite@gmail.com>
2018-05-25 10:07:35 -0700

    initial checkin

---
+++ CardGame/Assets/Scripts/Draggable.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+    public Transform returnParent = null;
+
+    GameObject placeholder = null;
+
+    //public enum Slot { DEBUNK, EVIDENCE, INVENTORY };
+    //public Slot typeOfItem = Slot.DEBUNK;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        placeholder = new GameObject();
+        placeholder.transform.SetParent(this.transform.parent);
+
+        LayoutElement le = placeholder.AddComponent<LayoutElement>();
+        le.preferredWidth = this.GetComponent<LayoutElement>().preferredWidth;
+        le.preferredHeight = this.GetComponent<LayoutElement>().preferredHeight;
+        le.flexibleHeight = 0;
+        le.flexibleWidth = 0;
+
+        placeholder.transform.SetSiblingIndex(this.transform.GetSiblingIndex());
+
+        returnParent = this.transform.parent;
+        this.transform.SetParent( this.transform.parent.parent );
+
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        this.transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        this.transform.SetParent( returnParent );
+        //this.transform.SetSiblingIndex(placeholder.transform.GetSiblingIndex());
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+        Destroy(placeholder);
+    }
+}
---
+++ CardGame/Assets/Scripts/DropZone.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour, IDropHandler  {
+
+    //public Draggable.Slot typeOfItem = Draggable.Slot.DEBUNK;
+    public void OnDrop(PointerEventData eventData)
+    {
+        Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+        if (d != null)
+        {
+            d.returnParent = this.transform;
+            //if (typeOfItem == d.typeOfItem || typeOfItem == Draggable.Slot.INVENTORY)
+            //{
+            //    d.returnParent = this.transform;
+            //}
+        }
+    }
+
+}
